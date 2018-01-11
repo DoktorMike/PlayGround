0f557df3a7d53a0f3195eb1ef6e5e8d4b2f40d78
Michael <micke.green@gmail.com>
2018-01-11 01:56:59 +0100

    Added more code.

--- juliaadventures/docbostonhousing.jl
+++ juliaadventures/docbostonhousing.jl
@@ -1,5 +1,5 @@
 #' ---
-#' title : Boston Housing example 
+#' title : Deep Neural Networks in Julia - Love at first sight?
 #' author : Michael Green
 #' date : January 10, 2018
 #' ---
@@ -12,9 +12,9 @@
 #' 
 #' When reading this it helps if you have a basic understanding of Neural networks and their mathematical properties. Mathwise, basic linear algebra will do for the majority of the post.
 #'
-#' # Short introductory example
+#' # Short introductory example - Boston Housing
 #'
-#' Instead of writing on and on about how cool this new language is I will just show you how quickly you can get a simple neural network up and running. The first example we will create is the BostonHousing dataset. This is baked into the deep learning library Knet. So let's start by fetching the data.
+#' Instead of writing on and on about how cool this new language is I will just show you how quickly you can get a simple neural network up and running. The first example we will create is the BostonHousing (http://www.cs.toronto.edu/~delve/data/boston/bostonDetail.html) dataset. This is baked into the deep learning library Knet. So let's start by fetching the data.
 
 using Knet;
 include(Knet.dir("data","housing.jl"));
@@ -36,13 +36,14 @@ function train(ω, data; lr=0.01)
     return ω
 end;
 
-#' Let's have a look at the first 5 variables of the data set.
+#' Let's have a look at the first 5 variables of the data set and their relation to the response that we would like to predict. The y axis in the plots is the response variable and the x axis the respective variables values. As you can see there are some correlations which seems to indicate some kind of relation though this is not definitive proof of a relation!
 
 using Plots;
 using StatPlots;
 
 include(Knet.dir("data","housing.jl"));
 x,y = housing();
+#plotly();
 gr();
 scatter(x', y[1,:], layout=(3,5), reg=true, size=(950,500))
 
@@ -146,3 +147,146 @@ using StatPlots
 @df errdf[5:60,:] plot(:Epoch, [:ValError, :TrnError], xlabel="Epoch", ylabel="Error", 
                        label=["Validation" "Training"], lw=3)
 
+#' # Deep convolutional networks on mnist
+#' The Boston Housing data set is, relatively speaking, a rather simple job for any well versed machine learning girl/guy. As such we need a little bit more bite to dig into what Julia can really do. So with no further ado we turn to convolutional neural networks. We will implement the LeNet which is an old fart in the game but nicely illustrates complexity and power.
+
+
+using Knet
+using Plots
+using StatPlots
+
+# Define necessary functions
+function predict(w, x0)
+    x1 = pool(relu.(conv4(w[1],x0) .+ w[2]))
+    x2 = pool(relu.(conv4(w[3],x1) .+ w[4]))
+    x3 = relu.(w[5]*mat(x2) .+ w[6])
+    return w[7]*x3 .+ w[8]
+end
+
+loss(ω, x, ygold) = nll(predict(ω, x), ygold)
+
+lossgradient = grad(loss)
+
+function train(model, data, optim)
+    for (x,y) in data
+        grads = lossgradient(model,x,y)
+        update!(model, grads, optim)
+    end
+end
+
+# Load the data
+include(Knet.dir("data","mnist.jl"))
+xtrn, ytrn, xtst, ytst = mnist()
+dtrn = minibatch(xtrn, ytrn, 100)
+dtst = minibatch(xtst, ytst, 100)
+
+# Initialise neural network
+ω = Any[xavier(Float32, 5, 5, 1, 20),  zeros(Float32, 1, 1, 20, 1),
+        xavier(Float32, 5, 5, 20, 50), zeros(Float32, 1, 1, 50, 1),
+        xavier(Float32, 500, 800),     zeros(Float32, 500, 1),
+        xavier(Float32, 10, 500),      zeros(Float32, 10, 1) ]
+
+o = optimizers(ω, Adam)
+
+#' The last part of the code that will actually do the training I will not execute because well that basically takes forever because I'm currently writing this on a non GPU based laptop :) I have reached out to the developer of Knet for an answer and it appears as though the entire library was primarily developed with a GPU computational platform in mind. This is not an unreasonable assumption due to the good fit between neural networks and GPU's. However, that doesn't help us when we want to do deep learning on our CPU based machine.
+
+# println((:epoch, 0, :trn, accuracy(ω, dtrn, predict), :tst, accuracy(ω, dtst, predict)))
+# for epoch=1:10
+#     train(ω, dtrn, o)
+#     println((:epoch, epoch, :trn, accuracy(ω, dtrn, predict), :tst, accuracy(ω, dtst, predict)))
+# end
+
+#' So armed with this information we look to another nice deep learning framework called MXNet. We can use this to define a convolutional neural network like we did with Knet. MXNet is a different framework so of course defining a network looks differently. 
+
+#' We start by loading the package in Julia and declaring a Variable in the MXNet framework called "data". This will serve as our reference to a data set that we wish to run the network on. Before we move on I should say that MXNet allows for two programming paradigms.
+#' - Imperative
+#' - Symbolic
+#' The symbolic paradigm is the one used by PyTorch and Tensorflow where you define a computational graph and then do operations on it. Thus nothing gets executed until you run data through it. Imperative works more like normal programming which is line by line basis. Both approches have their benefits and caveats and fortunately MXNet supports both. For the remainder of this post we will use the Symbolic interface.
+
+using MXNet
+data = mx.Variable(:data)
+
+#' We will again sort of replicate the LeNet model. The first convolutional layers is specified like this:
+conv1 = @mx.chain mx.Convolution(data, kernel=(5,5), num_filter=20)  =>
+mx.Activation(act_type=:tanh) =>
+mx.Pooling(pool_type=:max, kernel=(2,2), stride=(2,2))
+#' Notice the activation function as well as the pooling. We use 20 filters with a 5 by 5 kernel. On top of that we add a max pooling layer with another 2 by 2 kernel and a 2 by 2 stride. The second layer uses 50 filters with the same tactics. For both convolutional layers we use the tanh activation function.
+
+conv2 = @mx.chain mx.Convolution(conv1, kernel=(5,5), num_filter=50) =>
+mx.Activation(act_type=:tanh) =>
+mx.Pooling(pool_type=:max, kernel=(2,2), stride=(2,2))
+
+#' We end the network structure with two fully connected layers of size 500 and 10 respectively. Further the softmax function is applied to the output layer to make the outputs sum to one for each data point. This does NOT turn this into a probability mind you!
+
+fc1   = @mx.chain mx.Flatten(conv2) =>
+mx.FullyConnected(num_hidden=500) =>
+mx.Activation(act_type=:tanh)
+
+fc2   = mx.FullyConnected(fc1, num_hidden=10)
+
+lenet = mx.SoftmaxOutput(fc2, name=:softmax)
+
+#' ## Load data
+#' Before we move into the training of the newly specified network we need to fetch the data and convert it into a format that MXNet understands. It starts out by specifying some choices for the data fetch and then downloads MNIST into Pkg.dir("MXNet")/data/mnist if not exist. After that data providers are created which allows us to iterate over the data sets.
+
+# load data
+batch_size = 100
+data_name = :data
+label_name = :softmax_label
+filenames = mx.get_mnist_ubyte()
+train_provider = mx.MNISTProvider(image=filenames[:train_data],
+                                  label=filenames[:train_label],
+                                  data_name=data_name, label_name=label_name,
+                                  batch_size=batch_size, shuffle=true, flat=false, silent=true)
+eval_provider = mx.MNISTProvider(image=filenames[:test_data],
+                                 label=filenames[:test_label],
+                                 data_name=data_name, label_name=label_name,
+                                 batch_size=batch_size, shuffle=false, flat=false, silent=true)
+
+#' This gives us a training set and a validation set on the MNIST data from our MXNet package. To start using this data on our specified model we need to define the context in which we are going to train it. In this case, as I said before, we will use the CPU. If you have a GPU at your disposal I recommend you to exchange the context to mx.gpu() instead. Other than contextualising we need to define an optimizer and we'll stick with our stochastic gradient descent. The parameters are standard choices. Do feel free to play around with them.
+
+model = mx.FeedForward(lenet, context=mx.cpu())
+optimizer = mx.SGD(lr=0.05, momentum=0.9, weight_decay=0.00001)
+# mx.fit(model, optimizer, train_provider, n_epoch=10, eval_data=eval_provider)
+
+#' The actual fit is commented out as it still takes a long time on my machine. So please have a look at my repository for the full code. Thus, much like a cooking program I will restore some data from an offline run. I have loaded the probabilities for each digit class for each example along with the correct labels and an array of what I am calling outliers.
+
+using JLD
+probs = load("mymxnetresults.jld", "probs")
+labels = load("mymxnetresults.jld", "labels")
+outliers = load("mymxnetresults.jld", "outliers")
+
+#' Now we can use this new data to compute the accuracy which you can see below is quite ok on a validation set. This is by no means the best achievable on the MNIST but it suffices to illustrate what we can do with deep convolutional nets. 
+
+correct = 0
+for i = 1:length(labels)
+    # labels are 0...9
+    if indmax(probs[:,i]) == labels[i]+1
+        correct += 1
+    end
+end
+
+accuracy = 100correct/length(labels)
+println(mx.format("Accuracy on eval set: {1:.2f}%", accuracy))
+
+#' As stated the accuracy is quite fine. However, there's an interesting point I would like to make: What about the networks internal belief and confidence in what it's seeing? To highlight this we will look at the outliers which consists of all examples the neural net got wrong. 
+
+tmpdf=DataFrame(outliers)
+
+#' First we will plot the predicted class against the internal confidence of the network in it's classification. 
+
+scatter(convert(Array, tmpdf[2,:])', convert(Array, tmpdf[4,:])', ylim=(0, 1), 
+        ylabel="Likelihood", xlabel="Wrongly Predicted class")
+
+#' Now that we can see that the network is mostly quite convinced of it's findings even when it's dead wrong. It's above 40% sure every time. But let's do better and have a look at the histogram of likelihoods on the erroneosly classified digits.
+
+histogram(convert(Array, tmpdf[4,:])', xlims=(0,1), 
+          xlabel="Likelihood", ylabel="Frequency")
+
+#' In here it's clear to see that the vast majority of the mass is way above 80% which again shows how convinced the network is even when it's inherently wrong.
+
+#' # Conclusions
+#' Julia is an upcoming cool language that I believe will be well suited for deep learning applications and research. For a while longer I do believe that the early adopters of the language will be research engineers and scientists. There are a lot of things missing in the current packages compared to the Python and R universe but this is not strange given how young the language is. Further, Julia takes care of a lot of issues that Python and R currently have.
+#' So give it a go and see how you like it!
+
+#' Happy inferencing!
--- juliaadventures/mnistconvmxnet.jl
+++ juliaadventures/mnistconvmxnet.jl
@@ -42,11 +42,63 @@ model = mx.FeedForward(lenet, context=mx.cpu())
 optimizer = mx.SGD(lr=0.05, momentum=0.9, weight_decay=0.00001)
 
 # fit parameters
-mx.fit(model, optimizer, train_provider, n_epoch=20, eval_data=eval_provider)
+mx.fit(model, optimizer, train_provider, n_epoch=10, eval_data=eval_provider)
 
 # Visualize it
 open("network.dot", "w") do f write(f, mx.to_graphviz(lenet)) end
 
-# Predictions
+#--------------------------------------------------------------------------------
+# evaluate model
+
+# predictions
 probs = mx.predict(model, eval_provider)
+
+# collect all labels from eval data
+labels = Array[]
+for batch in eval_provider
+    push!(labels, copy(mx.get(eval_provider, batch, :softmax_label)))
+end
+labels = cat(1, labels...)
+
+# Now we use compute the accuracy
+correct = 0
+for i = 1:length(labels)
+    # labels are 0...9
+    if indmax(probs[:,i]) == labels[i]+1
+        correct += 1
+    end
+end
+accuracy = 100correct/length(labels)
+println(mx.format("Accuracy on eval set: {1:.2f}%", accuracy))
+
+# find silly examples
+outliers = []
+for i = 1:length(labels)
+    pred = indmax(probs[:,i])-1
+    prob = maximum(probs[:,i])
+    obs = labels[i]
+    if pred != obs push!(outliers, [i, pred, obs, prob]) end
+end
+
+using DataFrames
+using Plots
+tmpdf=DataFrame(outliers)
+scatter(convert(Array, tmpdf[2,:])', convert(Array, tmpdf[4,:])')
+histogram(convert(Array, tmpdf[4,:])', xlims=(0,1), 
+          xlabel="Likelihood", ylabel="Frequency")
+
+# Save files
+using JLD
+save("mymxnetresults.jld", 
+    # "model", model, "lenet", lenet, 
+     "probs", probs, "labels", labels,
+     "outliers", outliers, "tmpdf", tmpdf)
+
+using JLD, DataFrames, Knet, MXNet, Plots
+#model = load("mymxnetresults.jld", "model")
+#lenet = load("mymxnetresults.jld", "lenet")
+probs = load("mymxnetresults.jld", "probs")
+labels = load("mymxnetresults.jld", "labels")
+outliers = load("mymxnetresults.jld", "outliers")
+tmpdf = load("mymxnetresults.jld", "tmpdf")
 
