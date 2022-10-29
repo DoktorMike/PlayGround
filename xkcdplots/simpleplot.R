a1d977c7e85b06e290298eacac93ef5abdd08237
Michael Green <micke.green@gmail.com>
2022-10-29 16:22:50 +0200

    feat: added small plot for over and under fitting.

--- xkcdplots/simpleplot.R
+++ xkcdplots/simpleplot.R
@@ -1,6 +1,7 @@
 library(xkcd)
 library(sysfonts)
 library(ggplot2)
+library(patchwork)
 
 xrange <- range(mtcars[["mpg"]])
 yrange <- range(mtcars[["wt"]])
@@ -13,3 +14,38 @@ ggplot(mtcars, aes(x = mpg, y = wt)) +
 
 
 
+
+library(xkcd)
+mydf <- tibble(x = 50:100, y = sin(x / 50) + rnorm(length(x), 0, 0.025))
+xrange <- range(mydf[["x"]])
+yrange <- range(mydf[["y"]])
+
+p1 <- mydf |> ggplot(aes(x = x, y = y)) +
+  geom_point(size = 3) +
+  xkcdaxis(xrange, yrange) +
+  geom_smooth(method = lm, level = 0.999) +
+  theme_xkcd() +
+  theme(text = element_text(size = 20, family = "xkcd")) +
+  theme(axis.text.x = element_blank()) +
+  theme(axis.text.y = element_blank()) +
+  ggtitle("Underfit")
+p2 <- mydf |> ggplot(aes(x = x, y = y)) +
+  geom_point(size = 3) +
+  xkcdaxis(xrange, yrange) +
+  geom_smooth(method = lm, formula = y ~ poly(x, 15), level = 0.999) +
+  #geom_smooth(method = loess, span = 1/100000, n=100, level = 0.999) +
+  theme_xkcd() +
+  theme(text = element_text(size = 20, family = "xkcd")) +
+  theme(axis.text.x = element_blank()) +
+  theme(axis.text.y = element_blank()) +
+  ggtitle("Overfit")
+p3 <- mydf |> ggplot(aes(x = x, y = y)) +
+  geom_point(size = 3) +
+  xkcdaxis(xrange, yrange) +
+  geom_smooth(method = lm, formula = y ~ sin(x / 50), level = 0.999) +
+  theme_xkcd() +
+  theme(text = element_text(size = 20, family = "xkcd")) +
+  theme(axis.text.x = element_blank()) +
+  theme(axis.text.y = element_blank()) +
+  ggtitle("Perfect")
+p1 + p2 + p3
